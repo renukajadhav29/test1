956576d233fd17ff9f53bbd652d81fef6ac5234d
Renuka Jadhav <renukajadhavu@gmail.com>
2022-11-14 18:32:45 +0530

    java file added

---
+++ Demo.java
@@ -0,0 +1,19 @@
+class Demo
+{
+	int x;
+	public static void main(String args[])
+	{
+		int y=10;
+		Demo d=new Demo();
+		System.out.println(d.x+" "+y);
+		
+		byte a = 0b0000_0001; 
+System.out.println(~a);  
+
+	 int x1=1;    
+        if(x1)
+            System.out.println("True");
+        else
+            System.out.println("False");
+	}
+}
